03defe81dcb21ad95b875669daac22d2f6e58225
Irakly <ikachachava-7@mail.ru>
2024-07-15 23:19:08 +0300

    WB_APP_9

--- WB_APP/ContentView.swift
+++ WB_APP/ContentView.swift
@@ -2,16 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Animations()
     }
 }
-
 #Preview {
     ContentView()
 }
